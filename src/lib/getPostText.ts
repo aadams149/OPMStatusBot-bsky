3e29ce30d3f52cb389075b2cf595b38b6f2953ce
Alex Adams <70454645+aadams149@users.noreply.github.com>
2025-02-21 14:29:39 -0500

    Check cached status
    Add function to check previously retrieved status and post if current status is different. Runs every two hours. Also handle cases where

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,18 @@
 import { parseStringPromise } from 'xml2js';
+import * as fs from 'fs';
+const filePath: string = 'current_status.txt';
+
+function readFile(filePath: string): string | null {
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return data;
+  } catch (error) {
+    console.error(`Error reading file: ${error}`);
+    return null;
+  }
+}
+
+const fileContent = readFile(filePath);
 
 function cleanText(input: string): string {
   // Replace \r with \n
@@ -13,6 +27,8 @@ function cleanText(input: string): string {
 
 export default async function getPostText(): Promise<string> {
   // Fetch the XML file
+  // Test URL: https://www.opm.gov/xml/operatingstatus.xml?date=05/10/2013&markup=on
+  // Test URL (long message): https://www.opm.gov/xml/operatingstatus.xml?date=02/06/2025&markup=on
   const response = await fetch("https://www.opm.gov/xml/operatingstatus.xml", {
     headers: {
       Accept: "application/xml", // Accept XML response
@@ -35,16 +51,32 @@ export default async function getPostText(): Promise<string> {
       const appliesTo = currentStatus.AppliesTo?.[0]?.trim() || '';  // First element in AppliesTo array
       const longStatus = currentStatus.LongStatusMessage?.[0]?.trim() || '';  // First element in longStatusMessage array
       const longStatusMessage = cleanText(longStatus)
+      const shortSummary = currentStatus.OperatingStatus?.[0]?.trim() || '';
       const statusURL = currentStatus.Url?.[0]?.trim() || '';
       // Concatenate the extracted values into a single string with newline separator
       const result = `${appliesTo}\n\n${longStatusMessage}\n\n${statusURL}`;
-
+      const short_result = `${appliesTo}\n\n${shortSummary}\n\n${statusURL}`;
+ 
+      if(result != fileContent){
+        fs.writeFile(filePath, result, (err) => {
+          if (err) {
+            console.error("An error occurred while writing to the file:", err);
+          } else {
+            console.log("Text successfully written to", filePath);
+          }
+      })
+      console.log("Text is different, successfully updated");
+      if(result.length > 300){
+        console.log(short_result);
+        return short_result;
+      }else{
+      return result;}
+    } else {
+        console.error("Current status is identical to cached status");
+        return '';
+      };
       // Return the concatenated string
-      //if (result.length > 300) {
-      //  const shortSummary = currentStatus.StatusSummary.[0]?.trim() || '';
-       // const result = `${appliesTo}\n\n${shortSummary}\n\n${statusURL}`;
-     // }
-      return result;
+      
     } else {
       console.error("CurrentStatus not found in XML.");
       return '';  // Return an empty string if CurrentStatus is not found
